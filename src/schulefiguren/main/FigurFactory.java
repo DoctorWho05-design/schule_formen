a9ab9a57bf69bdd18a8905a15a5015a9f71b7759
DoctorWho05 <niklas.buecker.nb@gmail.com>
2023-12-08 14:10:24 +0100

    Update FigurFactory.java

--- src/schulefiguren/main/FigurFactory.java
+++ src/schulefiguren/main/FigurFactory.java
@@ -1,5 +1,80 @@
 package main;
 
+import java.awt.Point;
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class FigurFactory {
     
+    // Abstracte Klassen
+    private abstract class Figur {
+        protected String name;
+        protected Point position;
+        protected Color color;
+        protected boolean gefuellt;
+        protected Point size;
+
+
+        public Figur(String name) {
+            this.name = name;
+        }
+
+        public void setPosition(Point position) {
+            this.position = position;
+        }
+
+        public void setColor(Color color) {
+            this.color = color;
+        }
+
+        public void setFuelle(boolean gefuellt) {
+            this.gefuellt = gefuellt;
+        }
+
+        public void setSize(Point size) {
+            this.size = size;
+        }
+
+        public abstract void draw(Graphics g);    
+    }
+
+    // Child Klassen
+    // Kreis Klasse
+    private class Kreis extends Figur{
+
+        private int durchmesser;
+        public Kreis(){
+            super("Kreis");
+            this.durchmesser = size.x;
+        }
+
+        @Override
+        public void draw(Graphics g) {
+            g.setColor(color);
+            if (!gefuellt) {
+                g.drawOval(position.x, position.y, durchmesser, durchmesser);
+            } else {
+                g.fillOval(position.x, position.y, durchmesser, durchmesser);
+            }
+            
+        }
+    }
+
+    // Viereck Klasse
+    private class Viereck extends Figur{
+
+        public Viereck(){
+            super("Viereck");
+        }
+
+        @Override
+        public void draw(Graphics g) {
+            g.setColor(color);
+            if (!gefuellt) {
+                g.drawRect(position.x, position.y, size.x, size.y);
+            } else {
+                g.fillRect(position.x, position.y, size.x, size.y);
+            }
+        }
+    }
 }
